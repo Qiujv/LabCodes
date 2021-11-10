b47a2619295561532eb18aada48e23bda87cc51b
Qiujv <qiujv@outlook.com>
2021-11-11 00:35:55 +0800

    [new]: calculator add Transmon

--- labcodes/calculators.py
+++ labcodes/calculators.py
@@ -1,13 +1,18 @@
 """Modules contains class calculating parameters of models, e.g. Transmon, Gmon, and T coupler."""
 
-import attr
+import inspect
 import typing
 from functools import wraps
-import inspect
+
+import attr
 import numpy as np
+import scipy.constants as const
+
+Phi_0 = const.h / (2*const.e)  # Flux quantum.
 
 # A calculator class has two kind of attributes only: values and functions accepting 
 # only keyword arguments.
+# NOTE: Quantities here are in SI units unless noted.
 
 def use_attr_as_default(f):
     @wraps(f)
@@ -34,6 +39,53 @@ def use_attr_as_default(f):
         kw.update(kwargs)
         return f(self, **kw)
     return wrapped_f
+
+@attr.s(auto_attribs=True)
+class Capacitor(object):
+    c: float = 100e-15
+
+    @use_attr_as_default
+    def Ec(self, c, **kw):
+        return const.e**2 / (2*c)  # in J but not Hz.
+
+@attr.s(auto_attribs=True)
+class Junction(object):
+    w: float = 0.4e-6
+    h: float = 0.2e-6
+    # R*S = Oxidation constant, 650 Ohm*um^2 is and emprical value.
+    rs_const: float = 650 * 1e-6**2
+
+    @use_attr_as_default
+    def s(self, w, h, **kw):
+        return w*h
+
+    @use_attr_as_default
+    def rn(self, w, h, rs_const, **kw):
+        """Junction resistance."""
+        return rs_const / (w*h)
+
+    @use_attr_as_default
+    def Lj0(self, rn, **kw):
+        return 20.671 * rn/1e3 / (1.8*np.pi**2) * 1e-9  # Formula from ZYP.
+
+    @use_attr_as_default
+    def Ic(self, Lj0, **kw):
+        return Phi_0 / (2*np.pi*Lj0)
+
+    @use_attr_as_default
+    def Ej(self, Ic, **kw):
+        return Ic*Phi_0 / (2*np.pi)  # TODO: use Lj0 to compute it.
+
+@attr.s(auto_attribs=True)
+class Transmon(Junction, Capacitor):
+    @use_attr_as_default
+    def E10(self, Ec, Ej, **kw):
+        return np.sqrt(8*Ec*Ej) - Ec
+
+    @use_attr_as_default
+    def Em(self, m, Ec, Ej, **kw):
+        """Energy of levels, m=0, 1, 2..."""
+        return m*np.sqrt(8*Ec*Ej) - Ec/12 * (6*m**2 + 6*m + 3)
 
 @attr.s(auto_attribs=True)
 class TCoupler(object):
@@ -90,8 +142,8 @@ if __name__ == '__main__':
 
     tcplr = TCoupler()
     # With default it should be 1.5 and -1.38, same as @yan_tunable_2018.
-    print('The directive coupling strength (dimensionless) is:\n', tcplr.f_di)
-    print('The indirective coupling strength (dimensionless) is:\n', tcplr.f_in)
+    print('The directive coupling strength (dimensionless) is:\n', tcplr.f_di())
+    print('The indirective coupling strength (dimensionless) is:\n', tcplr.f_in())
 
     # With another set of values, This plot should recovers fig.2(b) in @yan_tunable_2018.
     wc = np.linspace(4.3e9, 7e9)
