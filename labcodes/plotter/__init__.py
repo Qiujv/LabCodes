dfb42711434c4de5211bf58d921e3823c28dab17
Qiujv <qiujv@outlook.com>
2022-02-24 12:45:00 +0800

    rename plotter.tomo into mat3d

--- labcodes/plotter/__init__.py
+++ labcodes/plotter/__init__.py
@@ -2,6 +2,6 @@
 """
 
 from labcodes.plotter.plot2d import plot2d_collection, plot2d_pcolormesh, plot2d_scatter
-from labcodes.plotter.tomo import plot_mat3d, plot_complex_mat3d
+from labcodes.plotter.mat3d import plot_mat3d, plot_complex_mat3d
 from labcodes.plotter.state_dis import plot_iq, plot_visibility
 from labcodes.plotter.misc import cursor
