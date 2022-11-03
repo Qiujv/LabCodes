21e0d86c14e92d35abcd694aa05216480a166f98
Qiujv <qiujv@outlook.com>
2022-11-03 23:58:19 +0800

    draft qpt_2q

--- labcodes/frog/ss_tomo.py
+++ labcodes/frog/ss_tomo.py
@@ -11,7 +11,12 @@ import labcodes.frog.pyle_tomo as tomo
 
 
 class single_shot_data:
-    def __init__(self, folder, id, suffix='csv', c_qubits=('q1', 'q2'), p_qubits=('q5',)):
+    """Load a single shot data lf, judge it with 0, 1 center stored in lf.conf.
+    then produce probs of p_qubits conditional to c_qubits.
+
+    Check .df for original data, check .probs for probilities table.
+    """
+    def __init__(self, folder, id, suffix='csv', c_qubits=('q1',), p_qubits=('q2',)):
         lf = fileio.LabradRead(folder, id, suffix=suffix)
         self.lf = lf
 
@@ -202,6 +207,7 @@ class qpt_1q:
     def __init__(self, folder, m, suffix='csv_complete'):
         selects = ['00', '01', '10', '11']
         self.selects = selects
+        fname = fileio.LabradRead(folder, m).name
 
         # Construct density matrices.
         rho_in = {
@@ -237,34 +243,36 @@ class qpt_1q:
             rho_out[select][init] = qst_out[init].rho(select)
         self.rho_out = rho_out
 
-        rho_out_ideal = {select: rho_in for select in selects}
-        # rho_out_ideal = {
-        #     '00': rho_in,
-        #     # '00': {  # after I, for 00
-        #     #     '0': rho(1,0),
-        #     #     'x': rho(1/np.sqrt(2),-1j/np.sqrt(2)),
-        #     #     'y': rho(1/np.sqrt(2), 1/np.sqrt(2)),
-        #     #     '1': rho(0,1),
-        #     # },
-        #     '01': {  # after Ypi, for 01
-        #         '0': rho(0,1),
-        #         'x': rho(1/np.sqrt(2), 1j/np.sqrt(2)),
-        #         'y': rho(1/np.sqrt(2), 1/np.sqrt(2)),
-        #         '1': rho(1,0),
-        #     },
-        #     '10': {  # after YpiXpi, for 10
-        #         '0': rho(1,0),
-        #         'x': rho(1/np.sqrt(2), 1j/np.sqrt(2)),
-        #         'y': rho(1/np.sqrt(2),-1/np.sqrt(2)),
-        #         '1': rho(0,1),
-        #     },
-        #     '11': {  # after Xpi, for 11
-        #         '0': rho(0,1),
-        #         'x': rho(1/np.sqrt(2), -1j/np.sqrt(2)),
-        #         'y': rho(1/np.sqrt(2), -1/np.sqrt(2)),
-        #         '1': rho(1,0),
-        #     }
-        # }
+        if 'tele_ss_ps' in fname.title.lower():
+            rho_out_ideal = {
+                '00': rho_in,
+                # '00': {  # after I, for 00
+                #     '0': rho(1,0),
+                #     'x': rho(1/np.sqrt(2),-1j/np.sqrt(2)),
+                #     'y': rho(1/np.sqrt(2), 1/np.sqrt(2)),
+                #     '1': rho(0,1),
+                # },
+                '01': {  # after Ypi, for 01
+                    '0': rho(0,1),
+                    'x': rho(1/np.sqrt(2), 1j/np.sqrt(2)),
+                    'y': rho(1/np.sqrt(2), 1/np.sqrt(2)),
+                    '1': rho(1,0),
+                },
+                '10': {  # after YpiXpi, for 10
+                    '0': rho(1,0),
+                    'x': rho(1/np.sqrt(2), 1j/np.sqrt(2)),
+                    'y': rho(1/np.sqrt(2),-1/np.sqrt(2)),
+                    '1': rho(0,1),
+                },
+                '11': {  # after Xpi, for 11
+                    '0': rho(0,1),
+                    'x': rho(1/np.sqrt(2), -1j/np.sqrt(2)),
+                    'y': rho(1/np.sqrt(2), -1/np.sqrt(2)),
+                    '1': rho(1,0),
+                }
+            }
+        else:
+            rho_out_ideal = {select: rho_in for select in selects}
         self.rho_out_ideal = rho_out_ideal
 
         Frho = self.empty_rho_dict()
@@ -300,7 +308,6 @@ class qpt_1q:
         self.Fchi = Fchi
 
         fchi_mean = np.mean(list(Fchi.values()))
-        fname = fileio.LabradRead(folder, m).name
         fname.title = fname.title + f', Fchi_mean={fchi_mean:.2%}'
         self.fname = fname
 
@@ -365,3 +372,187 @@ def rho(alpha, beta):
 
 def fidelity(rho, sigma):
     return np.real(np.trace(np.dot(rho, sigma)))
+
+class qst_2q:
+    def __init__(self, folder, m, suffix='csv_complete'):
+        # self.ops = list('ixy')
+        # self.ids = [id0, idx, idy]
+        self.suffix = suffix
+
+        # Do NOT keep ss datas to save memory.
+        datas = [
+            ss.single_shot_data(folder, id, suffix, c_qubits=['q2', 'q5'], p_qubits=['q1', 'q4'])
+            for id in range(m, m+9)
+        ]
+        # Fix logfiles missing |0> center or |1> center in conf.
+        passed_lf = None
+        for data in datas:
+            if data.probs is not None:
+                passed_lf = data.lf
+        if passed_lf is not None:
+            for data in datas:
+                if data.probs is None:
+                    data.lf.conf = passed_lf.conf
+                    data.construct()
+
+        self.probs = [ss_data.probs for ss_data in datas]
+    
+    def rho(self, select, ro_mat=None):
+        probs = [(df.loc[f'q2q5_{select}', 'q1q4_00'], 
+                  df.loc[f'q2q5_{select}', 'q1q4_01'], 
+                  df.loc[f'q2q5_{select}', 'q1q4_10'], 
+                  df.loc[f'q2q5_{select}', 'q1q4_11']) 
+                 for df in self.probs]
+
+        if ro_mat is not None:
+            for i, ps in enumerate(probs):
+                probs[i] = np.dot(np.linalg.inv(ro_mat), ps)
+            
+        rho = tomo.qst(np.array(probs), 'tomo2')
+        return rho
+
+from labcodes.frog import state_list
+
+class qpt_2q:
+    """Process single shot datas in gate teleportation experiments.
+    
+    Basically a function, but store returns in an object and provides some plot functions.
+    """
+    def __init__(self, folder, m, suffix='csv_complete'):
+        selects = ['00', '01', '10', '11']
+        self.selects = selects
+        init_states = [''.join(init) for init in product('0xy1', repeat=2)]
+        self.init_states = init_states
+
+        # Construct density matrices.
+        rho_in = dict(zip(init_states, state_list.rho_in))
+        self.rho_in = rho_in
+
+        qst_out = {
+            '00': qst_2q(m+0, suffix=suffix),
+            '0x': qst_2q(m+9, suffix=suffix),
+            '0y': qst_2q(m+18, suffix=suffix),
+            '01': qst_2q(m+27, suffix=suffix),
+
+            'x0': qst_2q(m+36, suffix=suffix),
+            'xx': qst_2q(m+45, suffix=suffix),
+            'xy': qst_2q(m+54, suffix=suffix),
+            'x1': qst_2q(m+63, suffix=suffix),
+
+            'y0': qst_2q(m+72, suffix=suffix),
+            'yx': qst_2q(m+81, suffix=suffix),
+            'yy': qst_2q(m+90, suffix=suffix),
+            'y1': qst_2q(m+99, suffix=suffix),
+
+            '10': qst_2q(m+108, suffix=suffix),
+            '1x': qst_2q(m+117, suffix=suffix),
+            '1y': qst_2q(m+126, suffix=suffix),
+            '11': qst_2q(m+135, suffix=suffix),
+        }
+        self.qst_out = qst_out
+
+        rho_out = self.empty_rho_dict()
+        for select, init in self.iter_rho_dict(rho_out):
+            rho_out[select][init] = qst_out[init].rho(select)
+        self.rho_out = rho_out
+
+        if 'tele_ss_ps' in fname.title.lower():
+            rho_out_ideal = {
+                select: dict(zip(init_states, state_list.rho_out_ideal[select]))
+                for select in selects
+            }
+        else:
+            rho_out_ideal = {
+                select: dict(zip(init_states, state_list.rho_out_ideal_fb[select]))
+                for select in selects
+            }
+        self.rho_out_ideal = rho_out_ideal
+
+        Frho = self.empty_rho_dict()
+        for select, init in self.iter_rho_dict(Frho):
+            Frho[select][init] = fidelity(rho_out[select][init], 
+                                          rho_out_ideal[select][init])
+        self.Frho = Frho
+        
+        # Construct process matrices.
+        chi = {
+            select: tomo.qpt(
+                [rho_in[init] for init in init_states], 
+                [rho_out[select][init] for init in init_states], 
+                'sigma2',
+            ) 
+            for select in selects
+        }
+        self.chi = chi
+
+        chi_ideal = {
+            select: tomo.qpt(
+                [rho_in[init] for init in init_states], 
+                [rho_out_ideal[select][init] for init in init_states], 
+                'sigma2',
+            ) 
+            for select in selects
+        }
+        self.chi_ideal = chi_ideal
+
+        Fchi = {select: fidelity(chi[select], chi_ideal[select]) 
+                for select in selects}
+        self.Fchi = Fchi
+
+        fchi_mean = np.mean(list(Fchi.values()))
+        fname = fileio.LabradRead(folder, m).name
+        fname.title = fname.title + f', Fchi_mean={fchi_mean:.2%}'
+        self.fname = fname
+
+    def empty_rho_dict(self):
+        return {select: {init: None for init in self.init_states} for select in self.selects}
+        
+    @staticmethod
+    def iter_rho_dict(rho_dict):
+        for select, d in rho_dict.items():
+            for init in d.keys():
+                yield select, init
+
+    def plot_chi(self, chi=None, title=None):
+        """Plot process matrices for all selects.
+        
+        Note:
+            - try `fig.savefig(fname.as_file_name()+'.png', bbox_inches='tight')`
+        """
+        if chi is None: chi = self.chi
+        if title is None: title = self.fname.as_plot_title(width=100)
+
+        fig = plt.figure(figsize=(15,4), tight_layout=False)
+        for i, (select, mat) in enumerate(chi.items()):
+            ax = fig.add_subplot(1, 4, i+1, projection='3d')
+            plotter.plot_mat3d(mat, ax=ax, colorbar=False)
+            ax.set_title(f'select={select}')
+
+        # Add colorbar.
+        fig.subplots_adjust(left=0.1)
+        cax = fig.add_axes([0.05, 0.15, 0.008, 0.6])
+        cbar = fig.colorbar(ax.collections[0], cax=cax, orientation='vertical')
+        cbar.set_ticks(np.linspace(-np.pi, np.pi, 5))
+        cbar.set_ticklabels((r'$-\pi$', r'$-\pi/2$', r'$0$', r'$\pi/2$', r'$\pi$'))
+        
+        fig.suptitle(title)
+        return fig
+
+    def plot_chi_4x2(self, chi=None, title=None):
+        if chi is None: chi = self.chi
+        if title is None: title = self.fname.as_plot_title(width=100)
+
+        fig = plt.figure(figsize=(14,8), tight_layout=False)
+        for i, (select, mat) in enumerate(chi.items()):
+            ax_r = fig.add_subplot(2, 4, 2*i+1, projection='3d')
+            ax_i = fig.add_subplot(2, 4, 2*i+2, projection='3d')
+            plotter.plot_complex_mat3d(mat, [ax_r, ax_i], cmin=-1, cmax=1, colorbar=False)
+            ax_i.set_title(f'select={select}')
+
+        fig.subplots_adjust(top=0.9)
+        cax = fig.add_axes([0.4, 0.53, 0.2, 0.01])
+        cbar = fig.colorbar(ax_r.collections[0], cax=cax, orientation='horizontal')
+        
+        fig.suptitle(title)
+        return fig
+
