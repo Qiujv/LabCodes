0fbfce33e3a411e0514385ff2e1220529cd5f58b
Qiujv <qiujv@outlook.com>
2023-02-06 10:18:38 +0800

    Update calc.py

--- labcodes/calc.py
+++ labcodes/calc.py
@@ -18,58 +18,85 @@ Phi_0 = const.h / (2*const.e)  # Flux quantum.
 # NOTE: Energy, angular velocity are converted into Hz.
 
 def dept(f):
-    """Wrap function to be a dependent on object attrbites.
+    """Wrap class function to be a dependent on object attrbites.
 
-    All the arguments will have their default value in object attributes, if found.
+    Try to find default value from object attributes for all arguments.
     """
     @wraps(f)
-    def wrapped_f(self, *args, **user_kw):
+    def wrapped_f(self, *user_args, **user_kw):
         debug = user_kw.get('debug', False)
-        kw = {}  # Function kwargs to be filled as required.
+        final_kw = {}  # Function kwargs to be filled as required.
         fsig = inspect.signature(f)
-        for arg_name in fsig.parameters.keys():
+        for i, arg_name in enumerate(fsig.parameters.keys()):
             # Search for required arguements one by one.
+            if i+1 <= len(user_args):
+                final_kw[arg_name] = user_args[i]
+                continue
             if arg_name in user_kw:
-                kw[arg_name] = user_kw[arg_name]
-            else:  # Search in self attributes.
-                try:
-                    attr = getattr(self, arg_name)
-                except AttributeError:
-                    continue  # Leave blank and proceed to and TypeError: missing argument in function call.
+                final_kw[arg_name] = user_kw[arg_name]
+                continue
+            if not hasattr(self, arg_name):
+                continue  # Leave blank and proceed to and TypeError: missing argument in function call.
 
-                if callable(attr):
-                    if attr == f: raise Exception('Loop evaluation!')  # TODO: Need a smarter checker.
-                    if debug is True: print(arg_name)
-                    kw[arg_name] = attr(**user_kw)  # Recursion here.
-                else:
-                    kw[arg_name] = attr
+            attr = getattr(self, arg_name)
+            if getattr(attr, 'is_dept', False):
+                if attr == f: raise Exception('Loop evaluation!')  # TODO: Need a smarter checker.
+                if debug is True: print(arg_name)
+                final_kw[arg_name] = attr(**user_kw)  # Recursion here.
+            else:
+                final_kw[arg_name] = attr
 
-        return f(self, *args, **kw)
+        return f(self, **final_kw)
+    wrapped_f.is_dept = True
     return wrapped_f
 
 class Calculator(object):
-    """Calculators are bacically a dict, with properties derived from that."""
+    """Abstract class for calculators.
+    
+    calculators are classes who has two types of members in their attributes: 
+    indept and dept. Indept are static quantities, while dept are dynamically 
+    calculated according to indept. Dept are defined as class methods decorated 
+    with '@dept'.
+
+    This class defines some functions that helps quickly setup or create an
+    snapshot for a calculator.
+
+    Note: Controdicts with attrs.
+    """
     def __init__(self, **kwargs):  # TODO: __init__(self, a=2, b=3, ...)
+        """Kwargs provided will be set to object attributes."""
         for k, v in kwargs.items():
             setattr(self, k, v)
 
-    def new(self, **kwargs):
+    def __getitem__(self, key):
+        v = getattr(self, key)
+        if getattr(v, 'is_dept', False):
+            return v()
+        else:
+            return v
+
+    def copy(self, **kwargs):
         kw = self.__dict__.copy()
         kw.update(kwargs)
         return self.__class__(**kw)
 
+    new = copy  # alias.
+
     @property
-    def indeps(self):
+    def snapshot(self):
         d = {}
         for k in dir(self):
-            if not k.startswith('_'):
-                if k != 'indeps':  # Avoid infinite recursion.
-                    v = getattr(self, k, None)
-                    if not callable(v):
-                        if isinstance(v, Calculator):
-                            d[k] = v.indeps  # Recursively solve the indeps.
-                        else:
-                            d[k] = v
+            if k.startswith('_'): continue
+            if k == 'snapshot': continue  # Avoid infinite recursion.
+            v = getattr(self, k, None)
+            if isinstance(v, Calculator):
+                d[k] = v.snapshot  # Recursively solve the indeps.
+            elif getattr(v, 'is_dept', False):
+                d[k] = v()
+            elif callable(v):
+                continue
+            else:
+                d[k] = v
         return d
 
 
@@ -144,7 +171,7 @@ class Transmon(Capacitor):
     def demo_Ej_vs_area(self, w=None):
         if w is None: w = np.linspace(50e-9,1e-6)
         jj = Junction(w=w)
-        qb = self.new(jj=jj)
+        qb = self.copy(jj=jj)
 
         fig, ax = plt.subplots(tight_layout=True)
         ax.set(
@@ -171,7 +198,7 @@ class Transmon(Capacitor):
     @dept
     def demo_Lq_vs_Lj0(self, Lj0=None):
         if Lj0 is None: Lj0 = np.linspace(8e-9,13e-9)
-        qb = self.new()
+        qb = self.copy()
         qb.jj.Lj0 = np.linspace(8e-9,13e-9)
 
         fig, ax = plt.subplots()
@@ -228,7 +255,7 @@ class RF_SQUID(Calculator):
 
     def demo(self, delta_ext=None):
         if delta_ext is None: np.linspace(0,2*np.pi)
-        squid = self.new(delta_ext=delta_ext)
+        squid = self.copy(delta_ext=delta_ext)
 
         fig, ax = plt.subplots()
         r = squid.L_linear / squid.Lj0()
@@ -292,7 +319,7 @@ class Gmon(RF_SQUID):
 
     def demo(self, delta_ext=None):
         if delta_ext is None: delta_ext = np.linspace(0,2*np.pi,200)
-        gmon = self.new(delta_ext=delta_ext)
+        gmon = self.copy(delta_ext=delta_ext)
 
         r = gmon.L_linear() / gmon.Lj0()
 
@@ -431,7 +458,7 @@ class ThermalDistribution(Calculator):
 
     def demo(self, t=None):
         if t is None: t = np.linspace(30e-3, 80e-3)
-        dist = self.new()
+        dist = self.copy()
 
         fig, ax = plt.subplots()
         ax.plot(t, dist.boltzmann(temp=t), label='Boltzmann dist.')
@@ -502,7 +529,7 @@ class Cable(Calculator):
 
     def demo(self, len=None):
         if len is None: len = np.linspace(0.3,1.2)
-        cb = self.new()
+        cb = self.copy()
 
         fig, ax = plt.subplots()
         ax.set(
