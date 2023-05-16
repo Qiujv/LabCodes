47acc1202ee86816f0aef5a844e85312386d8599
Qiujv <qiujv@outlook.com>
2023-05-16 17:35:31 +0800

    split calc.py into pkg

---
+++ labcodes/calc/__init__.py
@@ -0,0 +1,11 @@
+from labcodes.calc.base import Calculator, dept
+from labcodes.calc.qubits import (
+    Capacitor,
+    Junction,
+    Transmon,
+    RF_SQUID,
+    Gmon,
+    TCoupler,
+    ThermalDistribution,
+    Cable,
+)
