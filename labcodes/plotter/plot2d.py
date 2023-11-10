c5c376ef521746d5fd74338d6d6449661d9eaa2e
Qiujv <qiujv@outlook.com>
2023-11-10 22:21:17 +0800

    bugfix for plot2d_imshow

--- labcodes/plotter/plot2d.py
+++ labcodes/plotter/plot2d.py
@@ -1,19 +1,32 @@
 """Functions for general 2d plot."""
 
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
-from matplotlib.collections import PatchCollection
+import numpy as np
+import pandas as pd
 from labcodes.plotter import misc
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Rectangle
+
 
 def _shift_left(arr):
     arr = np.array(arr)
     shifted = np.hstack((arr[0]*2 - arr[1], arr[:-1]))
     return (arr + shifted) / 2
 
-def plot2d_collection(df, x_name, y_name, z_name, ax=None, cmin=None, cmax=None, 
-                      colorbar=True, cmap='RdBu_r', norm=None, **kwargs):
-    """Labrad style 2D pseudocolor plot 2D scan data.
+def plot2d_collection(
+    df: pd.DataFrame, 
+    x_name: str, 
+    y_name: str, 
+    z_name: str, 
+    ax: plt.Axes=None, 
+    cmin: float=None, 
+    cmax: float=None, 
+    colorbar: bool=True, 
+    cmap: str='RdBu_r',
+    norm=None,
+    **kwargs,
+):
+    """Plot z in color versus x and y.
 
     Data points are plotted as rectangular scatters with proper width and height 
     to fill the space. Inspired by https://stackoverflow.com/a/16240370
@@ -26,16 +39,13 @@ def plot2d_collection(df, x_name, y_name, z_name, ax=None, cmin=None, cmax=None,
         df: pandas.DataFrame.
         x_name, y_name, z_name: str, column name of data to plot.
         ax: matplotlib.axes, where to plot the figure.
-        cmin, cmax: float, used to set colorbar range. Also be accessed by `collection.set_clim()`.
-        colorbar: boolean, whether or not to plot colorbar.
+        cmin, cmax: float, set the colorbar range. Same as `collection.set_clim()`.
+        colorbar: bool, whether or not to plot colorbar.
         cmap: str or Colormap.
         norm: mpl.colors.Normalize or subsclasses. Scale of z axis, overriding 
             cmin, cmax.
             if None, use linear scale with limits in data.
         **kwargs: forward to mpl.collections.PathCollection.
-
-    Returns:
-        Axes with plot.
     """
     if ax is None:
         fig, ax = plt.subplots(tight_layout=True)
@@ -79,28 +89,32 @@ def plot2d_collection(df, x_name, y_name, z_name, ax=None, cmin=None, cmax=None,
         cbar = fig.colorbar(col, ax=ax, label=z_name, extend=extend_cbar, fraction=0.05)
     return ax
 
-def plot2d_imshow(df, x_name, y_name, z_name, ax=None, cmin=None, cmax=None, 
-                      colorbar=True, cmap='RdBu_r', norm=None, **kwargs):
-    """Plot 2D scan data with imshow.
+def plot2d_imshow(
+    df: pd.DataFrame, 
+    x_name: str, 
+    y_name: str, 
+    z_name: str, 
+    ax: plt.Axes=None, 
+    cmin: float=None, 
+    cmax: float=None, 
+    colorbar: bool=True, 
+    cmap: str='RdBu_r',
+    norm=None,
+):
+    """Plot z in color versus x and y with plt.imshow, with each data as a pixel in the image.
 
-    Each data point displayed as a pixel in image. Faster and save more space than plot2d_collection.
+    Faster than plot2d_collection, but assumes x, y are evenly spaced and no missing data.
     
     Args:
         df: pandas.DataFrame, container of data.
         x_name, y_name, z_name: str, column name of data to plot.
         ax: matplotlib.axes, where to plot the figure.
-        cmin, cmax: float, used to set colorbar range.
-            Can also be achieved by `collection.set_clim()`.
-        colorbar: boolean, whether or not to plot colorbar.
-            True by default.
+        cmin, cmax: float, set the colorbar range. Same as `collection.set_clim()`.
+        colorbar: bool, whether or not to plot colorbar.
         cmap: str or Colormap.
         norm: mpl.colors.Normalize or subsclasses. Scale of z axis, overriding 
             cmin, cmax.
             if None, use linear scale with limits in data.
-        **kwargs: forward to mpl.collections.PathCollection.
-
-    Returns:
-        Axes with plot.
     """
     if ax is None:
         fig, ax = plt.subplots(tight_layout=True)
@@ -112,13 +126,17 @@ def plot2d_imshow(df, x_name, y_name, z_name, ax=None, cmin=None, cmax=None,
     yuni = df[y_name].unique()
     xsize = xuni.size
     ysize = yuni.size
+    dx2 = (xuni[1] - xuni[0]) / 2
+    dy2 = (yuni[1] - yuni[0]) / 2
     xmax, xmin = xuni.max(), xuni.min()
     ymax, ymin = yuni.max(), yuni.min()
+    extent = [xmin - dx2, xmax + dx2, ymin - dy2, ymax + dy2]
     z = df[z_name].values.reshape(xsize, ysize).T
     norm, extend_cbar = misc.get_norm(z, cmin=cmin, cmax=cmax)
     cmap = plt.cm.get_cmap(cmap)
     colors = cmap(norm(z))
-    img = ax.imshow(colors, cmap=cmap, extent=[xmin, xmax, ymin, ymax], origin='lower', aspect='auto')
+    img = ax.imshow(colors, cmap=cmap, extent=extent, origin='lower', aspect='auto', interpolation='none')
+    img.set_norm(norm)
 
     ax.set(
         xlabel=x_name, 
@@ -126,13 +144,20 @@ def plot2d_imshow(df, x_name, y_name, z_name, ax=None, cmin=None, cmax=None,
     )
     if colorbar:
         # Way to remove colorbar: ax.images[-1].colorbar.remove()
-        cbar = fig.colorbar(img, ax=ax, label=z_name, extend=extend_cbar, fraction=0.05)
+        cbar = fig.colorbar(img, ax=ax, label=z_name, extend=extend_cbar, fraction=0.03)
     return ax
 
     
 if __name__ == '__main__':
-    from labcodes import fileio
-    lf = fileio.LabradRead('//XLD2-PC2/labRAD_data/crab.dir/220724.dir/661_0814.dir', 97)
-    ax = plot2d_collection(lf.df, 'zpa', 'drive_freq_GHz', 's1_prob')
+    import pandas as pd
+    x = np.linspace(0, 1, 21)
+    y = np.linspace(0, 1, 21)
+    x, y = np.meshgrid(x, y)
+    x = x.ravel()
+    y = y.ravel()
+
+    z = np.sin(x * 2 * np.pi) * np.cos(y * 2 * np.pi)
+    df = pd.DataFrame({"x": x, "y": y, "z": z})
+    plot2d_imshow(df, "x", "y", "z")
+    plot2d_collection(df.iloc[:-5,], "x", "y", "z")
     plt.show()
-    print('end')
